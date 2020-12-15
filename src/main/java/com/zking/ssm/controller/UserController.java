87dfa90d9ec27e87c79d1b4a646fc6884d086524
xiejinyou <2510862747@qq.com>
2020-12-15 15:35:42 +0800

    bug修复

--- src/main/java/com/zking/ssm/controller/UserController.java
+++ src/main/java/com/zking/ssm/controller/UserController.java
@@ -5,6 +5,7 @@ import com.zking.ssm.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.Id;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,8 @@ public class UserController {
     @Autowired
     private  IUserService userService;
 
-    @GetMapping("/login")
+//    @GetMapping("/login")
+    @RequestMapping("/login")
     public User  login (User user){
         User user1 = userService.selectByPrimaryKey(user.getId());
 
